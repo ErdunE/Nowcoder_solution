126c970eb64e055fe0a841886feb9c9fc151c438
Erdun E <eed950128@gmail.com>
2023-07-17 12:24:23 -0400

    BM101_较难_设计LFU缓存结构

--- 11. 模拟/BM101_较难_设计LFU缓存结构/Python_哈希表.py
+++ 11. 模拟/BM101_较难_设计LFU缓存结构/Python_哈希表.py
@@ -1,25 +1,11 @@
-## BM101_较难_设计LFU缓存结构
-
-#### 题目链接 [BM101_较难_设计LFU缓存结构](https://www.nowcoder.com/practice/93aacb4a887b46d897b00823f30bfea1?tpId=295&tqId=1006014&ru=/exam/oj&qru=/ta/format-top101/question-ranking&sourceUrl=%2Fexam%2Foj)
-
-![img](https://i.ibb.co/Zzvnpw9/20230712130842.png)
-
----
-## 解题思路
----
-### 使用方法：哈希表
----
-### 题目关键信息
-1. 实现LFU的set与get函数，且复杂度为O(1)
-2. 每次调用这两个函数会给一个频率赋值，超出长度则移除频率最少的，若有频率相同，则移除访问时间最早的
----
-### 解题步骤
-1. 设置 dict 记录 key-val 值,设置 cap 记录容量;
-2. 根据调用次数和调用次序进行排序,得出最终的最小key.
----
-
-### 最终代码
-```
+#
+# 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+#
+# lfu design
+# @param operators int整型二维数组 ops
+# @param k int整型 the k
+# @return int整型一维数组
+#
 class Cache:
     def __init__(self, k):
         self.cap = k
@@ -66,4 +52,3 @@ class Solution:
             elif operators[i][0] == 2:
                 res.append(cache.getKey(operators[i][1],i))
         return res
-```
