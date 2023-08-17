a0e64c365b77fa5110429190fd399b2c29ca1add
Erdun E <eed950128@gmail.com>
2023-08-17 16:18:43 -0400

    BM38_中等_在二叉树中找到两个节点的最近公共祖先

--- 03. 二叉树/BM38_中等_在二叉树中找到两个节点的最近公共祖先/Java_dfs.java
+++ 03. 二叉树/BM38_中等_在二叉树中找到两个节点的最近公共祖先/Java_dfs.java
@@ -1,32 +1,26 @@
-## BM38_中等_在二叉树中找到两个节点的最近公共祖先
-
-#### 题目链接 [BM38_中等_在二叉树中找到两个节点的最近公共祖先](https://www.nowcoder.com/practice/e0cc33a83afe4530bcec46eba3325116?tpId=295&tqId=1024325&ru=/exam/interview&qru=/ta/format-top101/question-ranking&sourceUrl=%2Fexam%2Finterview%3Forder%3D0)
-
-![img](https://i.ibb.co/xJvLSgC/20230810134041.png)
-
----
-## 解题思路
----
-### 使用方法：深度优先搜索（dfs）
----
-### 题目关键信息
-
-1. 给定一棵二叉树以及这棵树上的两个节点对应的val值 o1 和 o2，请找到 o1 和 o2 的最近公共祖先节点
-2. 二叉树非空，且每个节点值均不同
-
----
-### 解题步骤
-
-1. 利用dfs求得根节点到两个目标节点的路径：每次选择二叉树的一棵子树往下找，同时路径数组增加这个遍历的节点值。
-2. 一旦遍历到了叶子节点也没有，则回溯到父节点，寻找其他路径，回溯时要去掉数组中刚刚加入的元素。
-3. 然后遍历两条路径数组，依次比较元素值。
-4. 找到两条路径第一个不相同的节点即是最近公共祖先。
----
-
-### 最终代码
-```
 import java.util.*;
+
+/*
+ * public class TreeNode {
+ *   int val = 0;
+ *   TreeNode left = null;
+ *   TreeNode right = null;
+ *   public TreeNode(int val) {
+ *     this.val = val;
+ *   }
+ * }
+ */
+
 public class Solution {
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     * 
+     * @param root TreeNode类 
+     * @param o1 int整型 
+     * @param o2 int整型 
+     * @return int整型
+     */
     //记录是否找到到o的路径
     public boolean flag = false;
     //求得根节点到目标节点的路径
@@ -73,4 +67,3 @@ public class Solution {
         return res;
     }
 }
-```
